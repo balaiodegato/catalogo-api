ad92f4ccf54595e0c981ccb2ffc142776db66391
Felipe Machado <462154+felipou@users.noreply.github.com>
2020-04-27 11:45:54 -0300

    Add list database "cache" to avoid daily read limits

--- functions/index.js
+++ functions/index.js
@@ -4,6 +4,11 @@ const functions = require('firebase-functions');
 const express = require('express');
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const zlib = require('zlib');
+const util = require('util');
+
+const deflate = util.promisify(zlib.deflate);
+const inflate = util.promisify(zlib.inflate);
 
 admin.initializeApp(functions.config().firebase);
 
@@ -16,6 +21,7 @@ const main = express();
 app.set('etag', true);
 
 const collectionName = 'animals'
+const listCacheCollectionName = 'animalsListCache'
 
 main.use(cors())
 main.use('/v1/' + collectionName, app);
@@ -43,15 +49,68 @@ function docDataWithId(doc) {
     return docClone;
 }
 
-app.get('/', async (req, res) => {
-    const snapshot = await db.collection(collectionName).get();
+async function getListFromCache() {
+    const snapshot = await db.collection(listCacheCollectionName).get();
+    if (snapshot.docs.length === 0 || snapshot.docs.some(doc => doc.data() === null)) {
+        return null;
+    }
 
+    const inflatedDocs = await Promise.all(snapshot.docs.map(async doc => {
+        const inflatedData = await inflate(Buffer.from(doc.data().data, 'base64'))
+        return JSON.parse(inflatedData);
+    }));
+
+    const list = [].concat(...inflatedDocs);
+
+    return sortObj(list);
+}
+
+async function resetListCache(data) {
+    try {
+        const doc = db.collection(listCacheCollectionName).doc('0');
+        await doc.set({data});
+    } catch (err) {
+        console.error('Could not reset cache:', err)
+    }
+}
+
+async function saveToCache(data) {
+    try {
+        const deflatedData = await deflate(JSON.stringify(data))
+        await resetListCache(deflatedData.toString('base64'))
+    } catch (err) {
+        console.error('Could not save to cache:', err)
+    }
+}
+
+async function getListFromDb() {
+    const snapshot = await db.collection(collectionName).get();
     const list = [];
     snapshot.forEach((doc) => {
         list.push(docDataWithId(doc));
     });
+    return sortObj(list);
+}
 
-    res.status(200).send(sortObj(list));
+async function getList() {
+    const list = await getListFromCache();
+    if (list) {
+        return list;
+    } else {
+        const dbList = await getListFromDb();
+        await saveToCache(dbList);
+        return dbList;
+    }
+}
+
+app.get('/', async (req, res) => {
+    if (req.query.forcecache) {
+        res.status(200).send(await getListFromCache());
+    } else if (req.query.cache) {
+        res.status(200).send(await getList());
+    } else {
+        res.status(200).send(await getListFromDb());
+    }
 })
 
 app.post('/', async (req, res) => {
@@ -65,6 +124,7 @@ app.post('/saveAll', async (req, res) => {
         req.body.map(async pet => {
             await db.collection(collectionName).add(pet)
         })
+        await resetListCache(null);
         res.send('Pets successfully registered!');
     } catch(err) {
         res.status(500).send({ err: err.message })
@@ -76,12 +136,14 @@ app.patch('/:id', async (req, res) => {
     if (Object.keys(req.body || {}).length > 0) {
         await doc.update(req.body);
     }
+    await resetListCache(null);
     res.send(docDataWithId(await doc.get()));
 })
 
 app.put('/:id', async (req, res) => {
     const doc = db.collection(collectionName).doc(req.params.id);
     await doc.set(req.body);
+    await resetListCache(null);
     res.send(docDataWithId(await doc.get()));
 })
 
